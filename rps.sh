96915f604e437ce252a004f53939311f76ad9845
Ruben Paz <me@ruben.io>
2017-07-17 15:06:04 +0100

    [B] Fix problem parsing space input

--- rps.sh
+++ rps.sh
@@ -7,4 +7,4 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-node $DIR/dist/main/index.js $@
+node $DIR/dist/main/index.js "$@"
